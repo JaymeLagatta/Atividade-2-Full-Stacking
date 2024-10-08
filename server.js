a74b10c2497b53f1fc085ee2fe282490315f2cc9
JaymeLagatta <143770653+JaymeLagatta@users.noreply.github.com>
2024-10-08 19:34:16 -0300

    Add files via upload
    adicionado console.log para ajudar na identificação do erro no deploy

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-﻿require('dotenv').config();
+﻿console.log("Servidor está iniciando...");
+
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
